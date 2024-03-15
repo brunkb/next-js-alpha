a59f61ff9f978e7120d59de605ff2fd33768bcf4
Ben Brunk <brunkb@gmail.com>
2024-03-15 13:14:19 -0500

    Better layout for the add item page

--- app/components/todoFormAdv.tsx
+++ app/components/todoFormAdv.tsx
@@ -11,67 +11,55 @@ const TodoFormAdvanced = () => {
   const [data, formAction] = useFormState(handleAdvSubmit, initialState)
 
   return (
-    <div className="grid">
-      <div className="flow">
-        <form action={formAction}>
-          <div className="mt-2">
-            <label className="text-xl font-bold pl-4" htmlFor="todo">
-              Enter Task:
-            </label>
-            <span className="pl-20">
-              <input type="text" id="todo" name="nexttodo" required />
-            </span>
+    <div>
+      <form className="border" action={formAction}>
+        <div className="mt-2">
+          <label className="text-xl font-bold pl-4" htmlFor="todo">
+            Enter Task:
+          </label>
+          <span className="pl-20">
+            <input type="text" id="todo" name="nexttodo" required />
+          </span>
+        </div>
+        <div className="mt-4">
+          <label className="text-xl font-bold pl-4 align-top" htmlFor="todo">
+            Enter Description:
+          </label>
+          <span className="pl-3">
+            <textarea name="desc"></textarea>
+          </span>
+        </div>
+
+        <div className="mt-4 flex">
+          <label className="text-xl font-bold pl-4" htmlFor="todo">
+            Status:
+          </label>
+
+          <div className="ml-[6rem]">
+            <div className="radio ml-5">
+              <label>
+                <input className="mr-2" type="radio" name="rg1" value="not-done" />
+                Not Done
+              </label>
+            </div>
+            <div className="radio ml-5">
+              <label>
+                <input className="mr-2" type="radio" name="rg1" value="done" />
+                Done
+              </label>
+            </div>
+            <div className="radio ml-5">
+              <label>
+                <input className="mr-2" type="radio" name="rg1" value="blocked" />
+                Blocked
+              </label>
+            </div>
           </div>
-          <div className="mt-4">
-            <label className="text-xl font-bold pl-4 align-top" htmlFor="todo">
-              Enter Description:
-            </label>
-            <span className="pl-3">
-              <textarea name="desc"></textarea>
-            </span>
-          </div>
-         
-          <div className="mt-4">
-            <label className="text-xl font-bold pl-4" htmlFor="todo">
-              Status:
-            </label>
-          </div>
-          <div className="radio ml-5">
-            <label>
-              <input className="mr-2" type="radio" name="rg1" value="not-done" />
-              Not Done
-            </label>
-          </div>
-          <div className="radio ml-5">
-            <label>
-              <input className="mr-2" type="radio" name="rg1" value="done" />
-              Done
-            </label>
-          </div>
-          <div className="radio ml-5">
-            <label>
-              <input className="mr-2" type="radio" name="rg1" value="blocked" />
-              Blocked
-            </label>
-          </div>
-          <div className="mt-4">
-            <SubmitButton />
-          </div>
-        </form>
-      </div>
-      {/* <div className="mt-4">
-        <ul>
-          {data.map((item, index) => (
-            <li key={index}>
-              {item.todo}
-              {": "}
-              {item.description}
-              {'  '}
-              {item.status}
-            </li>
-          ))}
-        </ul>
-      </div> */}
+        </div>
+        <div className="mt-4 text-right">
+          <SubmitButton />
+        </div>
+      </form>
     </div>
   )
 }
--- app/todoList/page.tsx
+++ app/todoList/page.tsx
@@ -24,7 +24,7 @@ const MyTodos = async () => {
   const data = await getTodoList()
   return (
     <>
-      <div className="">
+      <div className="w-full">
         <h2 className="text-2xl">My Todo List</h2>
 
         {data.status === "error" && <div className="flex">An error occurred, try again later</div>}
@@ -34,17 +34,17 @@ const MyTodos = async () => {
             <table className="min-w-full border border-neutral-200 text-center text-sm font-light text-surface dark:border-white/10 dark:text-white">
               <thead className="border-b border-neutral-200 bg-neutral-50 font-medium dark:border-white/10 dark:text-neutral-800">
                 <tr className="p-4">
-                  <th className="text-left p-2">Todo Item</th>
-                  <th className="text-left">Description</th>
-                  <th className="text-left">Status</th>
+                  <th className="whitespace-nowrap text-left p-2">Todo Item</th>
+                  <th className="whitespace-nowrap text-left">Description</th>
+                  <th className="whitespace-nowrap text-left">Status</th>
                 </tr>
               </thead>
               <tbody>
               {data.alltodos.map((item: any) => (
                 <tr className="p-4">
-                  <td className="text-left p-2">{item.todo}</td>
-                  <td className="text-left">{item.desc}</td>
-                  <td className="text-left">{item.status === "done" && <CheckIcon className="h6 w-6"/>}</td> 
+                  <td className="whitespace-nowrap text-left p-2">{item.todo}</td>
+                  <td className="whitespace-nowrap text-left">{item.desc}</td>
+                  <td className="whitespace-nowrap text-left">{item.status === "done" && <CheckIcon className="h6 w-6"/>}</td> 
                 </tr>
               ))}
               </tbody>
